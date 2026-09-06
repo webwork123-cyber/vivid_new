5a35ac65de20fae4b69f84cd4424074afabd1d05
eliegnasr05-oss <https://github.com/eliegnasr05-oss/Alumix.git>
2026-09-06 20:48:52 +0300

    initial

--- BLL/Routes/powderCoating_Routes.js
+++ BLL/Routes/powderCoating_Routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const powderController = require("../Controller/powderCoating_controller");
+const powderController = require("../Controller/powderCoating_Controller");
 const authMiddleware = require("../../middleware/middleware");
 const upload = require("../../utils/uploads");
 
